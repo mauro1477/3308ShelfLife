4aa2408f26b4fe09badf88cc28358e75e4f6e4b9
Mauro Vargas <maurovargas7725@gmail.com>
2019-12-23 03:02:47 -0700

    changed back

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const http = require('http');
 const express = require('express')
 const bodyParser = require('body-parser')
-//const app = express()
-var connect = require("connect");
-
-var app = connect.createServer().use(connect.static(__dirname + '/views/login.html'));
-
+const app = express()
 const dbb = require('./resources/js/queries');
 var fs = require('fs');
 app.use(bodyParser.json());              // Add support for JSON encoded bodies
@@ -23,8 +19,7 @@ app.use(
 
 // set the view engine to ejs
 app.set('view engine', 'pug');
-//app.use(express.static(__dirname + '/login')); // This line is necessary for us to use relative paths and access our resources directory
-
+app.use(express.static(__dirname + '/')); // This line is necessary for us to use relative paths and access our resources directory
 if(process.env.ENVIRONMENT == 'PROD')
 {
   var db=ph(process.env.DATABASE_URL)
